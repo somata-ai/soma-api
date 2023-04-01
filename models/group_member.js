c383fee2096fa56ff4fe77c4b8c4f8f3ad64ef05
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-04-01 23:49:24 +0500

    Add model for group members

---
+++ models/group_member.js
@@ -0,0 +1,36 @@
+class GroupMember {
+  static create(newGroupMember, callback) {
+    connection.query(
+      "INSERT INTO GroupMember Set ?",
+      newGroupMember,
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        } else {
+          console.log("Created group member with id: ", result.insertId);
+          callback(null, { ...newGroupMember });
+        }
+      }
+    );
+  }
+
+  static delete(groupId, userId, callback) {
+    connection.query(
+      "DELETE FROM GroupMember WHERE group_id = ? AND user_id = ?",
+      [groupId, userId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        console.log(
+          `Deleted group member with group id: ${groupId} and user id ${userId}`
+        );
+        callback(null, { group_id: groupId, user_id: userId });
+      }
+    );
+  }
+}
+
+module.exports = GroupMember;
