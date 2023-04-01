def9ca40c7b53e143055f97e9378f6fff77094fc
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-04-01 23:05:55 +0500

    Add model for network models

---
+++ models/model.js
@@ -0,0 +1,94 @@
+class Model {
+  static create(newModel, callback) {
+    connection.query("INSERT INTO Model Set ?", newModel, (err, result) => {
+      if (err) {
+        callback(err, null);
+        return;
+      } else {
+        console.log("Created model with id: ", result.insertId);
+        callback(null, { model_id: result.insertId, ...newModel });
+      }
+    });
+  }
+
+  static findUserModels(userId, callback) {
+    connection.query(
+      "SELECT * FROM Model WHERE Model.user_id = ?",
+      [userId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        callback(null, result);
+      }
+    );
+  }
+
+  static findByName(name, callback) {
+    connection.query(
+      "SELECT * FROM Model WHERE Model.name LIKE ?",
+      ["%" + name + "%"],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        callback(null, result);
+      }
+    );
+  }
+
+  static findById(modelId, callback) {
+    connection.query(
+      "SELECT * FROM Model WHERE Model.model_id = ?",
+      [modelId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        callback(null, result);
+      }
+    );
+  }
+
+  static update(modelId, updatedModel, callback) {
+    connection.query(
+      "UPDATE Model SET name = ?, learning_rate = ?," +
+        " optimizer = ?, weights = ? WHERE Model.model_id = ?",
+      [
+        updatedModel.name,
+        updatedModel.learning_rate,
+        updatedModel.optimizer,
+        updatedModel.weights,
+        modelId,
+      ],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        console.log("Updated model with id: ", updatedModel.model_id);
+        callback(null, { ...updatedModel });
+      }
+    );
+  }
+
+  static delete(modelId, callback) {
+    connection.query(
+      "DELETE FROM Model WHERE model_id = ?",
+      [modelId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        console.log("Deleted model with id: ", modelId);
+        callback(null, { model_id: modelId });
+      }
+    );
+  }
+}
+
+module.exports = Model;
