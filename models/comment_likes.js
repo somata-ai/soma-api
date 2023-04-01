d3b492c4d3f07996f30d7aac352d1b4df8205bab
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-04-02 00:07:19 +0500

    Add model for comment likes

---
+++ models/comment_likes.js
@@ -0,0 +1,50 @@
+class CommentLikes {
+  static create(newCommentLike, callback) {
+    connection.query(
+      "INSERT INTO Comment_Likes Set ?",
+      newCommentLike,
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        } else {
+          console.log("Created comment like with id: ", result.insertId);
+          callback(null, { ...newCommentLike });
+        }
+      }
+    );
+  }
+
+  static findCommentLikes(commentId, callback) {
+    connection.query(
+      "SELECT * FROM Comment_Likes WHERE Comment_Likes.comment_id = ?",
+      [commentId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        callback(null, result);
+      }
+    );
+  }
+
+  static delete(commentId, userId, callback) {
+    connection.query(
+      "DELETE FROM Comment_Likes WHERE comment_id = ? AND user_id = ?",
+      [commentId, userId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        console.log(
+          `Deleted comment like with user id: ${userId} and comment id ${commentId}`
+        );
+        callback(null, { comment_id: commentId, user_id: userId });
+      }
+    );
+  }
+}
+
+module.exports = CommentLikes;
